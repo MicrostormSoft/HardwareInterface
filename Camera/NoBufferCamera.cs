f8db97bfa5402179401b09c1c8814587fa2d18f0
Developer_ken <dengbw01@outlook.com>
2021-07-20 15:22:31 +0800

    Initial Commit

---
+++ Camera/NoBufferCamera.cs
@@ -0,0 +1,41 @@
+﻿using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HardwareInterface
+{
+
+    public partial class NoBufferCamera
+    {
+        public VideoCapture cap;
+        public NoBufferCamera(int cam_id = 0, VideoCaptureAPIs vpis = VideoCaptureAPIs.V4L)
+        {
+            cap = new VideoCapture(cam_id, vpis)
+            {
+                ConvertRgb = true
+            };
+            cap.BufferSize = 1;
+        }
+
+        public new void Init()
+        {
+            while (!cap.IsOpened()) Thread.Sleep(0);
+        }
+
+        public new Mat GetLatestFrame()
+        {
+            Mat src = new Mat();
+            while (!cap.Read(src)) Thread.Sleep(0);
+            return src;
+        }
+
+        public void Abort()
+        {
+            cap.Dispose();
+        }
+    }
+}
